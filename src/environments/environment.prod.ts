4303226a0cd9bce38712948de72c32fde2c8a285
KevinMontalvo27 <montalvokevin375@gmail.com>
2026-02-24 08:39:35 -0700

    cambio de logo, de materia, login y boton e en

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend.edulors.org/'
+  apiUrl: 'https://backend.edulors.org/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'https://backend.edulors.org/'
+  production: true,
+  apiUrl: 'https://backend.edulors.org/',
 };
 
 /*
